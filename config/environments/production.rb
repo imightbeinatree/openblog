d339309ccfd7a8ad8a655fcc0dc01d99a298da3e
Michael Orr <michael@cloudspace.com>
2017-02-02 15:22:53 -0500

    let's try adding a few more of these settings for email for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,14 +40,17 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_mailer.default_options = { from: "noreply@example.com" }
+  
+  config.action_mailer.default_url_options = { host: 'desolate-eyrie-77999.heroku.com' }
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'desolate-eyrie-77999.heroku.com',
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'desolate-eyrie-77999.heroku.com',
+    authentication: :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
 
